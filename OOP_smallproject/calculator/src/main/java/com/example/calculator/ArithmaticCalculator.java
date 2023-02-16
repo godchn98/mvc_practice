5ebb326005cdbacd6143e1958741eb083142d7a9
Yeojin Park <godchn98@gmail.com>
2023-02-16 10:26:25 +0700

    #6 - operator test added

--- OOP_smallproject/calculator/src/main/java/com/example/calculator/ArithmaticCalculator.java
+++ OOP_smallproject/calculator/src/main/java/com/example/calculator/ArithmaticCalculator.java
@@ -1,5 +1,51 @@
 package com.example.calculator;
 
+import java.util.Arrays;
+
 public enum ArithmaticCalculator {
+    ADD("+"){
+        @Override
+        public int operandCalculate(int operand1, int operand2) {
+            return operand1 + operand2;
+        }
+    },
+
+    SUBTRACT("-") {
+        @Override
+        public int operandCalculate(int operand1, int operand2) {
+            return operand1 - operand2;
+        }
+    },
+
+    MULTIPLY("*") {
+        @Override
+        public int operandCalculate(int operand1, int operand2) {
+            return operand1 * operand2;
+        }
+    },
+
+    DIVIDE("/") {
+        @Override
+        public int operandCalculate(int operand1, int operand2) {
+            return operand1 / operand2;
+        }
+    };
+
+    private final String operator;
+
+    ArithmaticCalculator(String operator) {
+        this.operator = operator;
+    }
+
+    public abstract int operandCalculate(final int operand1, final int operand2);
+
+    public static int calculate(int operand1, String operator, int operand2){
+        ArithmaticCalculator arithmaticCalculator = Arrays.stream(values())
+                .filter(v -> v.operator.equals(operator))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Not valid operator"));
+        return arithmaticCalculator.operandCalculate(operand1, operand2);
+    }
+
 
 }
--- OOP_smallproject/calculator/src/main/java/com/example/calculator/Calculator.java
+++ OOP_smallproject/calculator/src/main/java/com/example/calculator/Calculator.java
@@ -2,16 +2,6 @@ package com.example.calculator;
 
 public class Calculator {
     public static int calculate(int opperand1, String operator, int opperand2) {
-        int result = 0;        
-        if(operator=="+" || operator=="-" ||operator=="*" ||operator=="/" ) {
-        switch (operator) {
-            case "+": result =  opperand1 + opperand2; break;
-            case "-": result = opperand1 - opperand2; break;
-            case "*": result = opperand1 * opperand2; break;
-            case "/": result = opperand1 / opperand2; break;
-        }}
-        else {throw new IllegalArgumentException("not a correct operator");}
-        
-        return result;
+        return ArithmaticCalculator.calculate(opperand1, operator, opperand2);
     }
 }
