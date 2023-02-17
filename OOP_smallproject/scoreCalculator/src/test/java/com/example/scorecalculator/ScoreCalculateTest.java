b38896acfbdbfcfd24ab4a6ccc0e7bab65fb3692
Yeojin Park <godchn98@gmail.com>
2023-02-17 11:48:36 +0700

    #6 - grade calculator test added

--- OOP_smallproject/scoreCalculator/src/test/java/com/example/scorecalculator/ScoreCalculateTest.java
+++ OOP_smallproject/scoreCalculator/src/test/java/com/example/scorecalculator/ScoreCalculateTest.java
@@ -1,4 +1,23 @@
 package com.example.scorecalculator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ScoreCalculateTest {
+
+    @DisplayName("input course, return score")
+    @Test
+    void scoreCalculator() {
+        List<Course> courses = List.of(new Course("OOP", 3, "A+"),
+                new Course("Database", 3, "A+"));
+
+        GradeCalculator gradeCalculator = new GradeCalculator(courses);
+        double gradeCalculate = gradeCalculator.calculate(courses);
+
+        assertThat(gradeCalculate).isEqualTo(4.5);
+    }
 }
